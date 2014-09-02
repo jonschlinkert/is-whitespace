73715e38e283309ef1a416bfd8d69e649c970e8e
Jordan Harband <ljharb@gmail.com>
2014-09-02 00:00:30 -0700

    Adding failing tests in some engines

--- test/test.js
+++ test/test.js
@@ -50,3 +50,14 @@ describe('when non-whitespace exists:', function () {
     expect(actual).to.eql(true);
   });
 });
+
+describe('ES5-compliant whitespace', function () {
+  it('should be true for all expected whitespace values', function () {
+    var actual = isWhitespace("\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF");
+    expect(actual).to.eql(true);
+  });
+
+  it('should not be true for the zero-width space', function () {
+    expect(isWhitespace('\u200b')).to.eql(false);
+  });
+});
