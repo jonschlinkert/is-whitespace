3386e377906019752300768dd5cc10266a2eaa0a
Jordan Harband <ljharb@gmail.com>
2014-09-02 00:00:42 -0700

    Fixing the failing tests.

--- index.js
+++ index.js
@@ -7,9 +7,11 @@
 
 'use strict';
 
+var regex = new RegExp('^[\\s\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF"]+$');
+
 module.exports = function isWhitespace(value) {
   if (typeof value === 'string') {
-    return (/^\s+$/).test(value);
+    return regex.test(value);
   }
   return false;
 };
